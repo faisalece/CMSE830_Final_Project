b3d9ca7d46e7899a4e0eb431872a59d5097e4d5d
Md Arifuzzaman Faisal <122495786+faisalece@users.noreply.github.com>
2023-12-03 13:50:30 -0500

    Update 2-Feature Analysis.py

--- pages/2-Feature Analysis.py
+++ pages/2-Feature Analysis.py
@@ -64,69 +64,27 @@ with dependents:
     st.write("##### Decision: Good feature")
 
 
-plot_options = ["Correlation Heat Map", "Joint Plot of Columns","Histogram of Column", "Pair Plot", "PairGrid Plot", "Box Plot of Column", "3D Scatter Plot"]
-selected_plot = st.sidebar.selectbox("Choose a plot type", plot_options)
+df_new = to_numeric(df)
+st.write("### Correlation Heatmap:")
+#plt.figure(figsize=(10, 10))
+sns.heatmap(df_new.corr(), annot=True, cmap='coolwarm')
+heatmap_fig = plt.gcf()  # Get the current figure
+st.pyplot(heatmap_fig)
 
-if selected_plot == "Correlation Heat Map":
-    st.write("Correlation Heatmap:")
-    #plt.figure(figsize=(10, 10))
-    sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
-    heatmap_fig = plt.gcf()  # Get the current figure
-    st.pyplot(heatmap_fig)
 
-elif selected_plot == "Joint Plot of Columns":
-    x_axis = st.sidebar.selectbox("Select x-axis", df.columns, index=0)
-    y_axis = st.sidebar.selectbox("Select y-axis", df.columns, index=1)
-    st.write("Joint Plot:")
-    jointplot = sns.jointplot(data = df, x=df[x_axis], y=df[y_axis], hue="Loan_Status")
-    #sns.scatterplot(data = df, x=df[x_axis], y=df[y_axis], hue="Potability", ax=ax)
-    st.pyplot(jointplot)
 
-elif selected_plot == "Histogram of Column":
-    column = st.sidebar.selectbox("Select a column", df.columns)
-    bins = st.sidebar.slider("Number of bins", 5, 100, 20)
-    st.write("Histogram:")
-    fig, ax = plt.subplots()
-    sns.histplot(data=df, x=column, hue="Loan_Status",bins=bins, kde=True)
-    st.pyplot(fig)
 
-elif selected_plot == "Pair Plot":
-    st.subheader("Pair Plot")
-    selected_box = st.multiselect('Select variables:', [col for col in df.columns if col != 'Loan_Status'])
-    selected_data = df[selected_box + ['Loan_Status']]  # Add 'Potability' column
-    all_columns = selected_data.columns
-    exclude_column = 'Loan_Status' 
-    dims = [col for col in all_columns if col != exclude_column]
-    fig = px.scatter_matrix(selected_data, dimensions=dims, title="Pair Plot", color='Loan_Status')
-    fig.update_layout(plot_bgcolor="white")  
-    st.plotly_chart(fig)
+column = st.sidebar.selectbox("Select a column", df_new.columns)
+bins = st.sidebar.slider("Number of bins", 5, 100, 20)
+st.write("Histogram:")
+fig, ax = plt.subplots()
+sns.histplot(data=df_new, x=column, hue="Loan_Status",bins=bins, kde=True)
+st.pyplot(fig)
 
-elif selected_plot == "PairGrid Plot":
-    st.subheader("Pair Plot")
-    selected_box = st.multiselect('Select variables:', [col for col in df.columns if col != 'Loan_Status'],default=['ph'])
-    selected_data = df[selected_box + ['Loan_Status']]  # Add 'Potability' column
 
-    # Create a PairGrid
-    g = sns.PairGrid(selected_data, hue='Loan_Status')
-    g.map_upper(plt.scatter)
-    g.map_diag(plt.hist, histtype="step", linewidth=2, bins=30)
-    g.map_lower(plt.scatter)
-    g.add_legend()
-
-    # Display the PairGrid plot
-    st.pyplot(plt.gcf())
-
-elif selected_plot == "Box Plot of Column":
-    column = st.sidebar.selectbox("Select a column", df.columns)
-    st.write("Box Plot:")
-    fig, ax = plt.subplots()
-    sns.boxplot(df[column], ax=ax)
-    st.pyplot(fig)
-
-elif selected_plot == "3D Scatter Plot":
-    x_axis = st.sidebar.selectbox("Select x-axis", df.columns, index=0)
-    y_axis = st.sidebar.selectbox("Select y-axis", df.columns, index=1)
-    z_axis = st.sidebar.selectbox("Select z-axis", df.columns, index=2)
-    st.subheader("3D Scatter Plot")
-    fig = px.scatter_3d(df, x=x_axis, y=y_axis, z=z_axis, color='Potability')
-    st.plotly_chart(fig)
+x_axis = st.sidebar.selectbox("Select x-axis", df_new.columns, index=0)
+y_axis = st.sidebar.selectbox("Select y-axis", df_new.columns, index=1)
+z_axis = st.sidebar.selectbox("Select z-axis", df_new.columns, index=2)
+st.subheader("3D Scatter Plot")
+fig = px.scatter_3d(df_new, x=x_axis, y=y_axis, z=z_axis, color='Loan_Status')
+st.plotly_chart(fig)
