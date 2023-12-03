46d39ec22d5b0d53130ccf4470138ef5a174dbf7
Md Arifuzzaman Faisal <122495786+faisalece@users.noreply.github.com>
2023-12-03 11:28:03 -0500

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load the dataset
+def load_data():
+    data = pd.read_csv('train.csv')
+    return data
+
+data = load_data()
+
+# Set page title and icon
+st.set_page_config(
+    page_title="Loan Status Analysis",
+    page_icon="💰",
+    layout="wide"
+)
+
+# Header
+st.title("Loan Status Analysis!")
+col1, col2 = st.columns([2, 5])
+with col1:
+    st.image("loan.jpg", caption="May I get a loan?", use_column_width=True)
+with col2:
+    st.write("In finance, a loan is when someone lends money to another person or organization. The person or organization receiving the money, known as the borrower, takes on a debt. This means they have to pay back the borrowed amount, plus an extra charge called interest. The borrower continues to pay until the entire borrowed amount is repaid. Loans serve as a common way for individuals and organizations to access financial support for different needs, making money more accessible for various purposes.")
+        
+
+
+#tabs
+intro_tab, goal_tab, describe_tab, significance_tab, con_tab = st.tabs(["Introduction", "Project Goal", "Describe the Dataset","Project Significance","Conclusion"])
+
+with intro_tab:
+    col3, col4 = st.columns([3, 2])
+    with col3:
+        st.write("When individuals apply for a loan from a bank, several key parameters are considered to assess their creditworthiness and determine the eligibility for the loan. Banks evaluate the applicant's financial stability, credit history, income, and debt levels. Additionally, factors such as employment history, loan purpose, and the applicant's relationship with the bank may also play a crucial role in the decision-making process. The overall goal is to gauge the borrower's ability to repay the loan in a timely manner. These parameters collectively help banks assess the risk associated with lending to a particular individual, ensuring responsible lending practices.")
+        st.write("Here are someparameters that banks commonly consider when evaluating loan applications:")
+        st.write("1. Credit Score")
+        st.write("2. Income")
+        st.write("3. Debt-to-Income Ratio")
+        st.write("4. Employment History")
+        st.write("5. Loan Purpose")
+        st.write("6. Family Size")
+        st.write("Etc.")
+    with col4:
+        st.image("loan_give.jpeg", caption="Here is your loan amount.", use_column_width=True)
+
