7c0df76193631b9e5f64885d9eb6e6973c2c6f4d
Md Arifuzzaman Faisal <122495786+faisalece@users.noreply.github.com>
2023-12-03 17:23:13 -0500

    Update 3-Logistic Regression.py

--- pages/3-Logistic Regression.py
+++ pages/3-Logistic Regression.py
@@ -1,149 +1,98 @@
-
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 
-st.image("H5.webp", use_column_width="always")
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
-ra=pd.read_csv('fifa.csv')
-rs=pd.read_csv('results.csv')
-gs=pd.read_csv('goalscorers.csv')
-conlist=sorted(list(rs['home_team'].unique()))
+# Load the dataset
+def load_data():
+    data = pd.read_csv('train.csv')
+    return data
 
+data = load_data()
 
+# Do not use inplace=True here
+df = data.drop('Loan_ID', axis=1)
 
+def to_numeric(df):
+    to_numeric = {'Male': 1, 'Female': 2,
+    'Yes': 1, 'No': 2,
+    'Graduate': 1, 'Not Graduate': 2,
+    'Urban': 3, 'Semiurban': 2, 'Rural': 1,
+    'Y': 1, 'N': 0,
+    '3+': 3}
 
-st.title('Head-to-head Statistics')
+    # No need for inplace=True here
+    data = df.applymap(lambda label: to_numeric.get(label) if label in to_numeric else label)
 
-st.write("Within this section, statistics for head-to-head football matches between any two countries are made available for analysis. These statistics are categorized based on the match location, encompassing games played at **home**, **away**, in **neutral** venues, and the **total** number of encounters. For each of these categories, the number of **wins**, **draws**, and **losses** is provided, as well as the corresponding **percentages**, which can represent each team's performance under distinct conditions.")
-
-col1, col2 = st.columns(2)
-
-with col1:
-    first = st.selectbox('First Team:',conlist, index=conlist.index('England'))
-
-
-with col2:
-    second = st.selectbox('Second Team:',conlist, index=conlist.index('Italy'))
-
-
-newra=rs[((rs['home_team'] == first) & (rs['away_team'] == second))|((rs['home_team'] == second) & (rs['away_team'] == first))]
-
-newrash=newra[['date','home_team','home_score','away_score','away_team','tournament','city']]
-
-
-
-def calculate_result(row):
-    if row['home_score'] > row['away_score']:
-        return 'Win'
-    elif row['home_score'] < row['away_score']:
-        return 'Lose'
-    else:
-        return 'Draw'
-
-newra['result'] = newra.apply(calculate_result, axis=1)
-
-T1=newra[(newra['home_team']==first) & (newra['neutral']==False)]
-T1['result'].value_counts()
-T2=newra[(newra['away_team']==first) & (newra['neutral']==False)]
-replacements = {'Lose': 'Win', 'Win': 'Lose'}
-T2['result'] = T2['result'].replace(replacements)
-T2['result'].value_counts()
-T3=newra[newra['neutral']==True]
-T3['result'].value_counts()
-df_T1 = pd.DataFrame(T1['result'].value_counts())
-df_T2 = pd.DataFrame(T2['result'].value_counts())
-df_T3 = pd.DataFrame(T3['result'].value_counts())
-combined_df = pd.concat([df_T1, df_T2, df_T3], axis=1)
-combined_df.columns = ['Home', 'Away', 'Neutral']
-combined_df = combined_df.fillna(0)
-combined_df['Total'] = combined_df['Home'] + combined_df['Away'] + combined_df['Neutral']
-desired_order = ['Win', 'Draw', 'Lose']
-combined_df = combined_df.reindex(desired_order)
+    return data
+df_num = to_numeric(df)
 
 
-# Radio button for selecting number or percent
-display_type = st.radio("Display Type", ["Number", "Percent"], horizontal=True)
-
-# Checkbox for selecting columns
-st.write("Select Columns")
-
-col1, col2, col3 ,col4  = st.columns(4)
-with col1:
-    show_home = st.checkbox("Home",value=True)
-with col2:
-    show_away = st.checkbox("Away",value=True)
-with col3:
-    show_neutral = st.checkbox("Neutral",value=True)
-with col4:
-    show_total = st.checkbox("Total",value=True)
-
-
-
-# Define custom colors for columns
-column_colors = {
-    'Home': 'slateblue',
-    'Away': 'chocolate',
-    'Neutral': 'gold',
-    'Total': 'gray'
-}
-
-# Filter the columns to display
-selected_columns = []
-selected_colors = []
-if show_home:
-    selected_columns.append('Home')
-    selected_colors.append(column_colors['Home'])
-if show_away:
-    selected_columns.append('Away')
-    selected_colors.append(column_colors['Away'])
-if show_neutral:
-    selected_columns.append('Neutral')
-    selected_colors.append(column_colors['Neutral'])
-if show_total:
-    selected_columns.append('Total')
-    selected_colors.append(column_colors['Total'])
-
-# Create a subset DataFrame based on the selected columns
-subset_df = combined_df[selected_columns]
-
-# Calculate percentages if needed
-if display_type == "Percent":
-    subset_df = subset_df.div(subset_df.sum(axis=0), axis=1) * 100
-
-# Create the bar plot
-ax = subset_df.plot(kind="bar", color=selected_colors, width=0.8, figsize=(10, 6),
-                    edgecolor='black', linewidth=1, legend=True)
-
-ax.spines['right'].set_visible(False)
-ax.spines['top'].set_visible(False)
-
-# Add labels on top of the "Total" bars
-
-
-if display_type == "Percent":
-    for p in ax.patches:
-        if p.get_height() > 0:
-            ax.annotate(f"{p.get_height():.1f}%", (p.get_x() + p.get_width() / 2., p.get_height()),ha='center', va='bottom')
-
-
-if display_type == "Number":
-    for p in ax.patches:
-        if p.get_height() > 0:
-            ax.annotate(f"{p.get_height():.0f}", (p.get_x() + p.get_width() / 2., p.get_height()),ha='center', va='bottom')
+def fill_data_median(df):
+    #Fill up data with median
+    df['Credit_History'].fillna(value=df['Credit_History'].median(),inplace=True)
+    df['Self_Employed'].fillna(value=df['Self_Employed'].median(),inplace=True)
+    df['LoanAmount'].fillna(value=df['LoanAmount'].median(),inplace=True)
+    df['Loan_Amount_Term'].fillna(value=df['Loan_Amount_Term'].median(),inplace=True)
+    df['Gender'].fillna(value=df['Gender'].median(),inplace=True)
+    return df
         
+def fill_data_KNN(df):
+    #Fill up data with KNN
+    my_imputer = KNNImputer(n_neighbors=5, weights='distance', metric='nan_euclidean')
+    df_repaired = pd.DataFrame(my_imputer.fit_transform(df_old), columns=df_old.columns)
+    return df_repaired
 
 
-plt.ylabel(display_type)
-plt.title(f'Stats of {first} in head to head matches against {second}')
-plt.xticks(rotation=0)
-st.pyplot(plt)
+df_numerical = fill_data_median(df_num)
 
-# Display the DataFrame using Streamlit
-st.dataframe(newrash, use_container_width=True)
+# Split the data into features (X) and target variable (y)
+y = df_numerical['Loan_Status']
+X = df_numerical.drop('Loan_Status', axis=1)
 
+# Split the data into training and testing sets
+start_state = 42
+test_fraction = 0.2
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_fraction, random_state=start_state)
 
+# Standardize the features using StandardScaler
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
 
-st.write("Upon close examination of these statistics, a discernible **home advantage** can be observed, especially when teams are closely matched in terms of skill and ability. This phenomenon becomes apparent when scrutinizing match results, where it becomes evident that teams tend to perform more favorably in the setting of their home ground. This observation underscores the noteworthy influence of **fan support** and the **familiarity** that teams possess with their home pitch, highlighting the challenges that come with **traveling** to face opponents on unfamiliar turf. ")
+# Create and train the Logistic Regression classifier
+lr_classifier = LogisticRegression()
+lr_model = lr_classifier.fit(X_train_scaled, y_train)
+
+# Evaluate the model on the test set
+test_score = lr_model.score(X_test_scaled, y_test)
+
+# Perform cross-validation
+cv_scores = cross_val_score(lr_classifier, X, y, cv=5)
+
+# Make predictions on the test set
+y_pred = lr_model.predict(X_test_scaled)
+
+# Generate and display the confusion matrix
+conf_mat = confusion_matrix(y_test, y_pred)
+
+# Streamlit app display
+st.title("Loan Status Prediction App")
+
+# Display test set score
+st.write(f"The score of the model on the test set is {test_score}")
+
+# Display cross-validation scores
+st.write(f"The cross-validation scores are: {cv_scores}")
+
+# Display the confusion matrix
+st.write("Confusion Matrix:")
+ConfusionMatrixDisplay(conf_mat, display_labels=['Approved', 'Not Approved']).plot()
+st.pyplot()
