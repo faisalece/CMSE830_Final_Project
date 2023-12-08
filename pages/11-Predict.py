3f13c587ed8d05957bac89b8c331adebc949c156
Md Arifuzzaman Faisal <122495786+faisalece@users.noreply.github.com>
2023-12-08 17:07:26 -0500

    Update 11-Predict.py

--- pages/11-Predict.py
+++ pages/11-Predict.py
@@ -1,46 +1,39 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression  # Import Logistic Regression
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report, accuracy_score
+from sklearn.linear_model import LogisticRegression
 
 # Load the dataset
+@st.cache  # Cache the dataset to avoid reloading on each Streamlit rerun
 def load_data():
     data = pd.read_csv('train.csv')
     return data
 
-data = load_data()
-
-# Do not use inplace=True here
-df = data.drop('Loan_ID', axis=1)
-
+# Data preprocessing functions
 def to_numeric(df):
     to_numeric = {'Male': 1, 'Female': 2,
-    'Yes': 1, 'No': 2,
-    'Graduate': 1, 'Not Graduate': 2,
-    'Urban': 3, 'Semiurban': 2, 'Rural': 1,
-    'Y': 1, 'N': 0,
-    '3+': 3}
-
-    # No need for inplace=True here
-    data = df.applymap(lambda label: to_numeric.get(label) if label in to_numeric else label)
-
-    return data
-
-df_num = to_numeric(df)
+                  'Yes': 1, 'No': 2,
+                  'Graduate': 1, 'Not Graduate': 2,
+                  'Urban': 3, 'Semiurban': 2, 'Rural': 1,
+                  'Y': 1, 'N': 0,
+                  '3+': 3}
+    return df.applymap(lambda label: to_numeric.get(label, label))
 
 def fill_data_mode(df):
-    # Fill up data with mode
-    null_cols = ['Credit_History', 'Self_Employed', 'LoanAmount','Dependents', 'Loan_Amount_Term', 'Gender', 'Married']
+    null_cols = ['Credit_History', 'Self_Employed', 'LoanAmount',
+                 'Dependents', 'Loan_Amount_Term', 'Gender', 'Married']
     for col in null_cols:
-        df[col] = df[col].fillna(df[col].dropna().mode().values[0])
+        df[col].fillna(df[col].dropna().mode().values[0], inplace=True)
     return df
 
 # Streamlit app display
-st.title("Loan Status Prediction!")  # Updated title
+st.title("Loan Status Prediction!")
+
+# Load and preprocess data
+data = load_data()
+df = data.drop('Loan_ID', axis=1)
+df_num = to_numeric(df)
 df_num_mode = fill_data_mode(df_num)
 
 # Split the data into features (X) and target variable (y)
@@ -48,21 +41,17 @@ y = df_num_mode['Loan_Status']
 X = df_num_mode.drop('Loan_Status', axis=1)
 
 # Split the data into training and testing sets
-start_state = 42
 test_fraction = 0.2
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_fraction, random_state=start_state)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_fraction, random_state=42)
 
 # Standardize the features using StandardScaler
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-# Create a logistic regression model
-logreg_model = LogisticRegression(random_state=start_state)
-
-# Train the model
+# Create and train a logistic regression model
+logreg_model = LogisticRegression(random_state=42)
 logreg_model.fit(X_train_scaled, y_train)
-
 
 # User input for prediction
 st.sidebar.header("User Input:")
@@ -71,20 +60,14 @@ married = st.sidebar.selectbox("Married", ["Yes", "No"])
 dependents = st.sidebar.selectbox("Dependents", ["0", "1", "2", "3+"])
 education = st.sidebar.selectbox("Education", ["Graduate", "Not Graduate"])
 self_employed = st.sidebar.selectbox("Self Employed", ["Yes", "No"])
-applicant_income = st.sidebar.slider("Applicant Income", 0, 80000, 0000)
-coapplicant_income = st.sidebar.slider("Coapplicant Income", 0, 50000, 0000)
+applicant_income = st.sidebar.slider("Applicant Income", 0, 80000, 0)
+coapplicant_income = st.sidebar.slider("Coapplicant Income", 0, 50000, 0)
 loan_amount = st.sidebar.slider("Loan Amount", 0, 800, 200)
 loan_amount_term = st.sidebar.slider("Loan Amount Term", 0, 500, 200)
 credit_history = st.sidebar.selectbox("Credit History", ["0", "1"])
 property_area = st.sidebar.selectbox("Property Area", ["Urban", "Semiurban", "Rural"])
 
 # Convert user input to numeric values
-to_numeric = {'Male': 1, 'Female': 2,
-              'Yes': 1, 'No': 2,
-              'Graduate': 1, 'Not Graduate': 2,
-              'Urban': 3, 'Semiurban': 2, 'Rural': 1,
-              '1': 1, '2': 2, '3+': 3,
-              '0': 0, '1': 1}
 user_input = pd.DataFrame({
     'Gender': [to_numeric[gender]],
     'Married': [to_numeric[married]],
@@ -100,9 +83,9 @@ user_input = pd.DataFrame({
 })
 
 # Make prediction
-user_input_scaled = StandardScaler().fit_transform(user_input)
+user_input_scaled = scaler.transform(user_input)
 prediction = logreg_model.predict(user_input_scaled)
-st.write(prediction)
+
 # Display prediction result
 st.header("Prediction Result:")
 if prediction[0] == 1:
