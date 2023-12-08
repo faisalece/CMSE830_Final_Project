916a6b7e14c79b0b018f16fc274fa1a7c2f426f8
Md Arifuzzaman Faisal <122495786+faisalece@users.noreply.github.com>
2023-12-08 16:56:35 -0500

    Update 11-Predict.py

--- pages/11-Predict.py
+++ pages/11-Predict.py
@@ -1,19 +1,68 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression  # Import Logistic Regression
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report, accuracy_score
 
-# Load the trained logistic regression model
-logreg_model = LogisticRegression(random_state=42)
-logreg_model.coef_ = np.array([[-0.45735262, 0.18649083, -0.03763093, -0.23950828, 0.05404476, -0.08057588,
-                                0.08625252, 0.17357327, -0.11847064, -0.04612795, 1.25295737]])
+# Load the dataset
+def load_data():
+    data = pd.read_csv('train.csv')
+    return data
 
-logreg_model.intercept_ = np.array([-0.10373654])
-logreg_model.classes_ = np.array([0, 1])
+data = load_data()
+
+# Do not use inplace=True here
+df = data.drop('Loan_ID', axis=1)
+
+def to_numeric(df):
+    to_numeric = {'Male': 1, 'Female': 2,
+    'Yes': 1, 'No': 2,
+    'Graduate': 1, 'Not Graduate': 2,
+    'Urban': 3, 'Semiurban': 2, 'Rural': 1,
+    'Y': 1, 'N': 0,
+    '3+': 3}
+
+    # No need for inplace=True here
+    data = df.applymap(lambda label: to_numeric.get(label) if label in to_numeric else label)
+
+    return data
+
+df_num = to_numeric(df)
+
+def fill_data_mode(df):
+    # Fill up data with mode
+    null_cols = ['Credit_History', 'Self_Employed', 'LoanAmount','Dependents', 'Loan_Amount_Term', 'Gender', 'Married']
+    for col in null_cols:
+        df[col] = df[col].fillna(df[col].dropna().mode().values[0])
+    return df
 
 # Streamlit app display
-st.title("Loan Status Prediction Using Logistic Regression")
+st.title("Loan Status Prediction!")  # Updated title
+df_num_mode = fill_data_mode(df_num)
+
+# Split the data into features (X) and target variable (y)
+y = df_num_mode['Loan_Status']
+X = df_num_mode.drop('Loan_Status', axis=1)
+
+# Split the data into training and testing sets
+start_state = 42
+test_fraction = 0.2
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_fraction, random_state=start_state)
+
+# Standardize the features using StandardScaler
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Create a logistic regression model
+logreg_model = LogisticRegression(random_state=start_state)
+
+# Train the model
+logreg_model.fit(X_train_scaled, y_train)
+
 
 # User input for prediction
 st.sidebar.header("User Input:")
